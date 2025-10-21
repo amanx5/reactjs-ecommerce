a48d10909578e84de229ddd51886aa7ab758c63a
amanx5 <110730645+amanx5@users.noreply.github.com>
2025-10-21 21:59:56 +0530

    Fetch Products from API

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,23 @@
 import './HomePage.css';
 import Header from '../../components/Header.jsx';
 import Product from './Product.jsx';
-import { products } from './data/products.js';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function HomePage() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		setData();
+	}, []);
+
+	async function setData() {
+		const response = await axios.get('http://localhost:3000/api/products');
+		const data = response.data;
+		if (data?.length) {
+			setProducts(data);
+		}
+	}
 
 	return (
 		<>
