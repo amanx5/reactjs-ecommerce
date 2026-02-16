1552318b5324fe20b49e9b2ea0de193d8c782010
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-02-16 15:21:55 +0530

    [ui] Wrap UI files in ui folder

---
+++ ui/src/pages/notfound/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import './NotFoundPage.css';
+import Header from '../../components/Header';
+export default function NotFoundPage() {
+	return (
+		<>
+			<Header />
+			<div className='notfound-page'>
+				<div className='page-title'>404 Page Not Found</div>
+			</div>
+		</>
+	);
+}
